431ba4dcae48af5aa78ae25dc2a4046480aa0c20
himanegi <himanegi2468@gmail.com>
2025-01-31 15:12:06 +0530

    chore: file cleanup

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -2,37 +2,9 @@ import { Module } from '@nestjs/common';
 import { MessagesController } from './messages.controller';
 import { MessagesService } from './messages.service';
 import { MessagesRepository } from './messages.repository';
-import {
-  AuthGuard,
-  KeycloakConnectModule,
-  ResourceGuard,
-  RoleGuard,
-} from 'nest-keycloak-connect';
-import { KeycloakConfigService } from 'src/config/keycloak-config.service';
-import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [
-    KeycloakConnectModule.registerAsync({
-      useClass: KeycloakConfigService,
-    }),
-  ],
   controllers: [MessagesController],
-  providers: [
-    {
-      provide: APP_GUARD,
-      useClass: AuthGuard,
-    },
-    {
-      provide: APP_GUARD,
-      useClass: ResourceGuard,
-    },
-    {
-      provide: APP_GUARD,
-      useClass: RoleGuard,
-    },
-    MessagesService,
-    MessagesRepository,
-  ],
+  providers: [MessagesService, MessagesRepository],
 })
 export class MessagesModule {}

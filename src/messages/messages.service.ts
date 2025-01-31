34a818014965f346964eaf8104898bd7995424be
himanegi <himanegi2468@gmail.com>
2025-01-31 10:50:40 +0530

    add nest project

---
+++ src/messages/messages.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { MessagesRepository } from './messages.repository';
+
+@Injectable()
+export class MessagesService {
+  constructor(public messagesRepo: MessagesRepository) {}
+
+  async findOne(id: string) {
+    return await this.messagesRepo.findOne(id);
+  }
+  findAll() {
+    return this.messagesRepo.findAll();
+  }
+  create(content: string) {
+    return this.messagesRepo.create(content);
+  }
+}
